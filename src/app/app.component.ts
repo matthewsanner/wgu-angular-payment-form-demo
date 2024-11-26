25c511e635f7de1a499e1375a21f697d450a1826
Matthew Sanner <matthewsannerdev@gmail.com>
2024-11-25 18:29:40 -0600

    updates template and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConvertPipe } from './convert.pipe';
+import {
+  CurrencyPipe,
+  DatePipe,
+  NgIf,
+  PercentPipe,
+  SlicePipe,
+  TitleCasePipe,
+} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ConvertPipe,
+    TitleCasePipe,
+    DatePipe,
+    CurrencyPipe,
+    PercentPipe,
+    NgIf,
+    SlicePipe,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'wgu-angular-payment-form-demo';
+  title = 'Payment Form';
+  name!: string;
+  date!: string;
+  amount!: number;
+  interest!: number;
+  suffix: boolean = false;
+
+  onNameChange(event: Event) {
+    this.name = (event.target as HTMLInputElement).value;
+  }
+
+  onDateChange(event: Event) {
+    this.date = (event.target as HTMLInputElement).value;
+  }
+
+  onAmountChange(event: Event) {
+    this.amount = parseFloat((event.target as HTMLInputElement).value);
+  }
+
+  onInterestChange(event: Event) {
+    this.interest = parseFloat((event.target as HTMLInputElement).value);
+  }
 }
